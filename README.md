8628487684e0efdda2d70633c355361ee52e7c92
Johan <johan@refinemusic.io>
2017-10-25 20:57:20 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # graphql-apollo-json-to-schema
-recursively converts a JSON object to a Schema
+recursively converts a JSON object to an Apollo Schema
+read more about schemas [here](http://dev.apollodata.com/tools/graphql-tools/generate-schema.html)
+
+can be used directly with output from MongoDB
+
+usage:
+
+```javascript
+paramters: name (String)
+JSON: Json Object,
+skip: array *Optional
+
+
+converter(name, JSON, skip(array) )
+
+```
+
+example:
+
+```javascript
+
+const converter = require('graphql-apollo-json-to-schema')
+
+const input = {
+  name:'Dr Steve Brule',
+  profession:'Doctor',
+  quotes: ['Bringo!','Jackprot!'],
+  age:27,
+  password:'nohunks',
+  apperances: {
+    checkItOut: {
+      type:'tv show',
+      rating:'11/10',
+      date: new Date()
+    },
+
+  }
+}
+
+console.log(converter('User',input,['password']))
+//`
+// OutOput:
+// type User {
+//   name: String
+//   profession: String
+//   quotes: [String]
+//   age: Int
+//    apperances: UserApperances
+//  }
+// 
+//  type UserApperances {
+//    checkItOut: UserApperancesCheckitout
+//  }
+// 
+//  type UserApperancesCheckitout {
+//   type: String
+//   rating: String
+//   date: String
+//  }
+// `
+
+```
+
+**Note on Arrays containing Objects:**
+The first item in an array will be picked and used, if subsequent items contains other items they will be lost.
+
---
+++ index.js
@@ -0,0 +1,100 @@
+const _ = require('lodash');
+const typeOf = require('typeof')
+
+const ObjectId = (id) =>{
+  return id;
+}
+const ISODate = (date) =>{
+  return date;
+}
+
+
+const parseRootOutput = (name, obj, skip = []) => {
+  const rootOutput = readObject(name, obj, skip);
+  const items = recursiveRead(name, obj);
+  let results = `
+    ${rootOutput}
+  `;
+  _.forEach(items, (item)=>{
+      const itemClass = readObject(item.name, _.get(obj, item.path), skip);
+      results += itemClass;
+  })
+
+  return results;
+
+}
+
+
+const recursiveRead = function(name, obj, path='') {
+  const Objects = [];
+  const extra = path ? '.':'';
+  _.forEach(Object.keys(obj), (item)=>{
+    const itemName = name+_.capitalize(item)
+    let itemPath = path+extra+item;
+      if(typeOf(obj[item]) === 'object') {
+        Objects.push({name:itemName, path:itemPath });
+        const items = recursiveRead(itemName, obj[item], itemPath)
+        if(items) Objects.push(items);
+      }
+      if(typeOf(obj[item]) === 'array') {
+        const subItem = obj[item][0]
+          if(typeOf(subItem) === 'object') {
+            itemPath = itemPath+`[0]`;
+            const items = recursiveRead(itemName, subItem, itemPath)
+            if(items) Objects.push(items);
+          }
+      }
+      function parseObject(itemName, obj, itemPath) {
+
+      }
+  })
+  if(Objects.length) return _.flatten(Objects);
+}
+
+
+const readObject = function (name, obj, skip) {
+  let returnObj = '';
+
+  const length = Object.keys(obj).length;
+returnObj+=`\n type ${name.replace(/[^a-zA-Z ]/, "")} { \n`
+  _.forEach(Object.keys(obj), (item, index)=>{
+    if(skip.includes(item)) return
+    if(item === '_id') returnObj = setRow(item, 'ID!', returnObj);
+    else  {
+      switch (typeOf(obj[item])) {
+        case 'string':
+            returnObj = setRow(item, 'String', returnObj);
+          break;
+        case 'number':
+            returnObj = setRow(item, 'Int', returnObj);
+          break;
+        case 'array':
+          returnObj = setRow(item, '[String]', returnObj);
+        break;
+        case 'boolean':
+          returnObj = setRow(item, 'String', returnObj);
+        break;
+        case 'date':
+          returnObj = setRow(item, 'String', returnObj);
+        break;
+        case 'object':
+        returnObj += `   ${item}: ${name+_.capitalize(item)} \n`
+        break;
+      }
+    }
+  });
+  return setClose(returnObj);
+}
+
+
+const setClose = function(rootOutput) {
+  return rootOutput+=` }\n`; }
+
+const setRow = function(item, value, rootOutput) {
+  return rootOutput += `  ${item}: ${value} \n`
+}
+
+module.exports = function(name, json, skip) {
+  if(!name || !json) return new Error('missing parameters');
+  return parseRootOutput(name, json, skip);
+}
